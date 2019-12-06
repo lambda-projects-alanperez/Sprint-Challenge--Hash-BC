d5b5ac81c7a1fb14c42fd45b31a10c4dc0e47f4f
Alan Perez <alanp9six@gmail.com>
2019-12-06 12:44:51 -0600

    x

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,19 +12,33 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    indexes = []
+#  Hint:  You may not need all of these.  Remove the unused functions.
+from hashtables import (HashTable,
+                        hash_table_insert,
+                        hash_table_remove,
+                        hash_table_retrieve,
+                        hash_table_resize)
+
+
+def get_indices_of_item_weights(weights, length, limit):
+    ht = HashTable(16)
+
+    """
+    YOUR CODE HERE
+    """
+    answer = []
     for i in range(length):
         hash_table_insert(ht, weights[i], i)
 
     for j in range(length):
         result = hash_table_retrieve(ht, limit-weights[j])
         if result != None:
-            indexes.append(result)
+            answer.append(result)
 
-    if indexes and indexes[0] != indexes[1]:
-        return indexes
-    elif indexes and indexes[0] == indexes[1]:
-        return [indexes[1], 0]
+    if answer and answer[0] != answer[1]:
+        return answer
+    elif answer and answer[0] == answer[1]:
+        return [answer[1], 0]
     else:
         return None
 
